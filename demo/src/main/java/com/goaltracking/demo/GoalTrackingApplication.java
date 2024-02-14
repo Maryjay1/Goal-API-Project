f8a7b7e2f9131693e27ff3e3f6cc573103815fe1
Mary Joseph <mary_jay@icloud.com>
2024-02-14 07:23:41 -0400

    setting up folders

---
+++ demo/src/main/java/com/goaltracking/demo/GoalTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.goaltracking.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalTrackingApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/goaltracking/demo/GoalTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goaltracking.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
